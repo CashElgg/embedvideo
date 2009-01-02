2c3795f5dd7ba884146df84455d637d3f9d57843
cash <cash.costello@gmail.com>
2009-01-02 20:37:18 +0000

    replaced input views with custom inputs due to bug in core and possible issues with tinymce

--- views/default/widgets/embedvideo/edit.php
+++ views/default/widgets/embedvideo/edit.php
@@ -1,17 +1,12 @@
 <?php
 
 ?>
-	<p>
-		<?php echo elgg_echo('embedvideo:url'); ?><br />
-		<?php echo elgg_view('input/text', array(
-									  'internalname' => 'params[url]',
-								    'value' => $vars['entity']->url,
-													) ); ?>
-	</p>
-	<p>
-		<?php echo elgg_echo('embedvideo:comment'); ?><br />
-		<?php echo elgg_view('input/longtext', array(
-									  'internalname' => 'params[comment]',
-								    'value' => $vars['entity']->comment,
-													) ); ?>
-  </p>	
+  <p>
+    <?php echo elgg_echo('embedvideo:url'); ?><br />
+    <input type="text" name="params[url]" value="<?php echo htmlentities($vars['entity']->url); ?>" class="input-text" />
+  </p>
+  
+  <p>
+    <?php echo elgg_echo('embedvideo:comment'); ?><br />
+    <textarea class="input-textarea" name="params[comment]" ><?php echo htmlentities($vars['entity']->comment); ?></textarea>
+  </p>  
