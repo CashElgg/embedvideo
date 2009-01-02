4ed0c7b854fa581211604477689fc00ed96db130
cash <cash.costello@gmail.com>
2009-01-02 22:17:23 +0000

    formatting and selecting entire textbox on click in video url

--- views/default/widgets/embedvideo/edit.php
+++ views/default/widgets/embedvideo/edit.php
@@ -1,12 +1,18 @@
 <?php
 
+  /**
+	 * embed video widget edit page 
+	 */
+
 ?>
+  <p>Youtube and Google videos are supported.</p>
   <p>
     <?php echo elgg_echo('embedvideo:url'); ?><br />
-    <input type="text" name="params[url]" value="<?php echo htmlentities($vars['entity']->url); ?>" class="input-text" />
+    <input onclick="this.select();" type="text" name="params[url]" value="<?php echo htmlentities($vars['entity']->url); ?>" class="input-text" />
   </p>
   
   <p>
-    <?php echo elgg_echo('embedvideo:comment'); ?><br />
-    <textarea class="input-textarea" name="params[comment]" ><?php echo htmlentities($vars['entity']->comment); ?></textarea>
+    <?php echo elgg_echo('embedvideo:comment'); ?> (html tags are allowed)<br />
+    <textarea class="input-textarea" name="params[comment]" style="height: 100px;" ><?php echo htmlentities($vars['entity']->comment); ?></textarea>
   </p>  
+
