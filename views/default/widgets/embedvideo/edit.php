ae6aad6bafafd9508bc243ec003baf8ce170183e
cash <cash.costello@gmail.com>
2009-03-19 00:54:57 +0000

    overriding bug in default theme css regarding input width

--- views/default/widgets/embedvideo/edit.php
+++ views/default/widgets/embedvideo/edit.php
@@ -14,16 +14,16 @@
   <p>
     <input type="hidden" name="params[url_hash]" id="params[url_hash]" value="<?php echo $url_hash; ?>" /> 
     <?php echo elgg_echo('embedvideo:url'); ?><br />
-    <input onclick="this.select();" type="text" name="params[url]" value="<?php echo htmlentities($vars['entity']->url); ?>" class="input-text" />
+    <input onclick="this.select();" type="text" name="params[url]" style="width: 96%;" value="<?php echo htmlentities($vars['entity']->url); ?>" class="input-text" />
   </p>
   
   <p>
     <?php echo elgg_echo('embedvideo:title'); ?><br />
-    <textarea class="input-textarea" name="params[videotitle]" style="height: 20px;" ><?php echo htmlentities($vars['entity']->videotitle); ?></textarea>
+    <textarea class="input-textarea" name="params[videotitle]" style="height: 20px; width: 96%;" ><?php echo htmlentities($vars['entity']->videotitle); ?></textarea>
   </p>  
 
   <p>
     <?php echo elgg_echo('embedvideo:comment'); echo elgg_echo('embedvideo:tags_instruct'); ?> <br />
-    <textarea class="input-textarea" name="params[comment]" style="height: 100px;" ><?php echo htmlentities($vars['entity']->comment); ?></textarea>
+    <textarea class="input-textarea" name="params[comment]" style="height: 100px; width: 96%;" ><?php echo htmlentities($vars['entity']->comment); ?></textarea>
   </p>  
 
